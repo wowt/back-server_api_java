f6a547a60846dd0b4596187403ea84fbd3c41f29
wanghongcheng <hongchengwang@dealmoon.com>
2018-02-07 16:02:31 +0800

    编辑工具类

--- service_api_java/src/main/java/com.fruitmall/fmsdk/SdkServer.java
+++ service_api_java/src/main/java/com.fruitmall/fmsdk/SdkServer.java
@@ -7,8 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.annotation.PropertySource;
 
 import java.util.TimeZone;
 
