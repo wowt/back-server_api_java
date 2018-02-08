23d0deee96bf177e664ada44697a2c0bd09cd097
wanghongcheng <2466022993@qq.com>
2018-02-09 01:33:29 +0800

    s

--- service_api_java/src/main/java/com.fruitmall/fmsdk/SdkServer.java
+++ service_api_java/src/main/java/com.fruitmall/fmsdk/SdkServer.java
@@ -4,14 +4,19 @@ package com.fruitmall.fmsdk;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 import java.util.TimeZone;
 
+
+@ImportResource("applicationContext.xml")
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-@Configuration
+@SpringBootApplication
 public class SdkServer {
 
     static Logger logger = LoggerFactory.getLogger(SdkServer.class);
--- service_api_java/src/main/java/com.fruitmall/model/CartModel.java
+++ service_api_java/src/main/java/com.fruitmall/model/CartModel.java
@@ -1,5 +1,6 @@
 package com.fruitmall.model;
 
+import com.fruitmall.model.entity.CartEntity;
 import com.fruitmall.model.fruitmall.mapper.CartEntityMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,5 +10,10 @@ public class CartModel {
 
     @Autowired
     private CartEntityMapper mapper;
+
+    public void getById() {
+        CartEntity entity = mapper.getById(1);
+        System.out.println(entity);
+    }
 
 }
